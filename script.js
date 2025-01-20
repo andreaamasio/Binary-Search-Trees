fbc83f166555624817696ec4dbbd41e0285282af
andreaamasio <101635283+andreaamasio@users.noreply.github.com>
2025-01-20 13:51:03 +0100

    Feat insert method

--- script.js
+++ script.js
@@ -10,6 +10,29 @@ class Tree {
     let array = sortAndRemoveDuplicates(arr)
     this.root = buildTree(array)
   }
+  insert(value) {
+    let current = this.root
+    while (current) {
+      if (value > current.data) {
+        if (current.right === null) {
+          current.right = new Node(value)
+          return
+        }
+        current = current.right
+      } else if (value < current.data) {
+        if (current.left === null) {
+          current.left = new Node(value)
+          return
+        }
+        current = current.left
+      } else return //value is already in the tree
+    }
+  }
+  checkIfLeaf(node) {
+    if (node.left === null && node.right === null) {
+      return true
+    } else return false
+  }
 }
 function sortAndRemoveDuplicates(array) {
   let set = new Set(array)
@@ -21,7 +44,6 @@ function sortAndRemoveDuplicates(array) {
   return sortedArray
 }
 function buildTree(array) {
-  //console.log(`array;${array}`)
   if (array.length === 0) {
     return null
   }
@@ -29,20 +51,18 @@ function buildTree(array) {
     return new Node(array[0])
   }
   let midIndex = Math.floor(array.length / 2)
-  //console.log(`midIndex;${midIndex}`)
+
   let root = array[midIndex]
-  console.log(`root:${root}`)
+
   let left = array.slice(0, midIndex)
   let right = array.slice(midIndex + 1)
-  console.log(`left:${left}`)
-  console.log(`right:${right}`)
 
-  let leftTree = buildTree(array.slice(0, midIndex))
-  let rightTree = buildTree(array.slice(midIndex + 1))
+  let leftTree = buildTree(left)
+  let rightTree = buildTree(right)
 
   return new Node(root, leftTree, rightTree)
 }
-const arr = [9, 1, 2, 6, 7, 3, 4, 5]
+const arr = [9, 7, 88, 52, 87, 1, 2, 5]
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -56,27 +76,8 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
   }
 }
-const prettyPrintStr = (node, prefix = "", isLeft = true) => {
-  if (node === null) {
-    return ""
-  }
-  let result = ""
-  if (node.right !== null) {
-    result += prettyPrint(
-      node.right,
-      `${prefix}${isLeft ? "│   " : "    "}`,
-      false
-    )
-  }
-  result += `${prefix}${isLeft ? "└── " : "┌── "}${node.data}\n`
-  if (node.left !== null) {
-    result += prettyPrint(
-      node.left,
-      `${prefix}${isLeft ? "    " : "│   "}`,
-      true
-    )
-  }
-  return result
-}
+
 const myTree = new Tree(arr)
+console.log(prettyPrint(myTree.root))
+myTree.insert(4)
 console.log(prettyPrint(myTree.root))
